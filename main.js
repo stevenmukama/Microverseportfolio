769552aafa8ae2868da5f321da4b9b8b2dedd01f
Mukama Steven <stevenmukama1@gmail.com>
2022-11-19 19:27:12 +0200

    Added required changes

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// const namesarray = ['secondimage', 'imagehovered'];
-// const descriptionarray = ['css style', 'js program'];
-// const readmore = ['readmore'];
-
-// const dynamic = document.querySelector('.gridcontainer2');
-// // eslint-disable-next-line no-plusplus
-// for (let i = 0; i < namesarray.length; i++) {
-//   const fetch = document.querySelector('.gridcontainer2').innerHTML;
-//   const bgimg = document.getElementById(`cards${i}`);
-//   dynamic.innerHTML = `<div id="cards${i}" class="boxes">
-//   <div class="box-content">
-//   <h2>${namesarray[i]}</h2>
-//   <p>  ${descriptionarray[i]} </p>
-//   <p> <a class="showmore" href="#">${readmore[0]}</a></p>
-//   <img ${bgimg} />
-//   </div>
-//   </div>${(fetch)}`;
-//   bgimg.style.backgroundImage = `url('./images/${namesarray[i]}.png)`;
-// }
-
 const menuBtn = document.querySelector('.navbarmenu');
 const menuItems = document.querySelector('.itemslist');
 const listItems = document.querySelectorAll('.listitem');
@@ -63,6 +43,19 @@ function closeMenu() {
 }
 
 listItems.forEach((item) => item.addEventListener('click', closeMenu));
+
+const projectfirst = [
+  {
+    title: 'Multi Post Stories',
+    popupdescription: 'Multi Post Stories',
+    objectdescription: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
+    description: 'A daily selection of privately personalized reads;no accounts or sign-ups required. has been the industrys standard',
+    technology: ['css', 'html', 'Bootstrap', 'Ruby'],
+    languages: ['html', 'Bootstrap', 'Ruby on rails'],
+    image: './images/Snapshootportfolio.png',
+    livelink: 'https://stevenmukama.github.io/Microverseportfolio/',
+    sourceLink: 'https://github.com/stevenmukama/Microverseportfolio',
+  }];
 
 const projectCard = [
   {
@@ -114,24 +107,48 @@ const projectCard = [
     objectdescription: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
     technology: ['html', 'Bootstrap', 'Ruby'],
     languages: ['html', 'Bootstrap', 'Ruby on rails'],
-    // image: 'SVG/bgw2.svg',
     livelink: 'https://stevenmukama.github.io/Microverseportfolio/',
     sourceLink: 'https://github.com/stevenmukama/Microverseportfolio',
   },
   {
-    title: 'Website Protfolio',
+    title: 'Website Portfolio',
     popupdescription: 'Multi Post Stories',
     description: 'A daily selection of privately personalized reads no accounts or sign-ups required. has been the industrys standard',
     objectdescription: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita',
     technology: ['html', 'Bootstrap', 'Ruby'],
     languages: ['html', 'Bootstrap', 'Ruby on rails'],
-    // image: 'SVG/w6bgdt.png',
     livelink: 'https://stevenmukama.github.io/Microverseportfolio/',
     sourceLink: 'https://github.com/stevenmukama/Microverseportfolio',
   },
 ];
 
 const workcards = document.querySelector('.grids');
+
+const firstcard = document.querySelector('.gridcontainer1');
+
+projectfirst.forEach((work) => {
+  firstcard.innerHTML += ` <div class="card1">
+    <div class="cardsection">
+    <img src="./images/firstimage.png" class="firstimageproject">
+    </div>
+    <div class="firstprojectdescription">
+    <h2 class="stories">${work.title}</h2>
+      <p class="selection">
+        ${work.description}
+      </p>
+      <ul class="listbutton">
+        <li class="button1" >${work.technology[0]}</li>
+        <li class="button2" >${work.technology[1]}</li>
+        <li class="button3" >${work.technology[2]}</li>
+        <li class="button4"> ${work.technology[3]}</li>
+      </ul>
+      <button type="button" class="button5 cardbtn"  > See Project</button>
+    </div>
+    <p class="card-liveLink" hidden>${work.livelink}</p>
+    <p class="card-sourceLink" hidden>${work.sourceLink}</p>
+                <p class="card-image" hidden>${work.image}</p>
+  </div>`;
+});
 
 projectCard.forEach((work, index) => {
   workcards.innerHTML += ` <div id="id${index + 1}" class="cards">
@@ -180,7 +197,7 @@ for (let i = 0; i < btn.length; i += 1) {
     </div>
     <div class="modal-bottom" id="modal-bottom">
       <div class="project-img">
-        <img src="images/Snapshootportfolio.png" alt=" Image" class="project-img-source">
+        <img src="./images/Snapshootportfolio.png" alt=" Image" class="project-img-source">
       </div>
       <div class="project-details">
         <div class="project-desc">
@@ -190,11 +207,11 @@ for (let i = 0; i < btn.length; i += 1) {
         <div class="project-links">
           <a href="" id="project-link-1" class="project-link-source">
            See Source
-          <img src="/images/github.svg" alt="see-project-icon">
+          <img src="./images/github.svg" alt="see-project-icon">
         </a>
         <a href="" id="project-link-1" class="project-link-live">
         See live
-       <img src="/images/live.svg" alt="see-project-icon">
+       <img src="./images/live.svg" alt="see-project-icon">
      </a>
         </div>
       </div>
@@ -214,27 +231,3 @@ window.onclick = (event) => {
     modal.style.display = 'none';
   }
 };
-
-// forma validation
-
-const emailE = document.querySelector('#mail'); // focus on email ID
-const form = document.querySelector('.formcontainter'); // focus on the form
-const error = document.querySelector('#error'); // focus on area below button to store error messsage
-
-function validator(emailAcquire) {
-  if (emailAcquire.match(/^[a-z@.0-9-_]*$/)) {
-    return true;
-  }
-  return false;
-}
-
-form.addEventListener('submit', (e) => {
-  if (validator(emailE.value)) {
-    error.textContent = '';
-  } else {
-    e.preventDefault();
-    emailE.style.border = '3px solid red';
-    error.style.display = 'flex';
-    error.textContent = 'Error! Please Use Lowercase Email.';
-  }
-});
